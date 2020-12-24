bfaa9dc5cd6358452ca6f8c81797f524b2458fc6
queenarzoo <arzooqueen850@gmail.com>
2020-12-23 22:45:45 -0800

    first commit

---
+++ Cutiepii_Robot/modules/CutieSpecial.py
@@ -0,0 +1,36 @@
+
+__help__ = """
+*Gif and Images:*
+ • `/pat`*:* pats a user by a reply to the message.
+ • `/neko`*:* sends random neko image
+ • `/hug`*:* sends random hug gif
+ • `/tickle`*:* sends random tickle gif
+ • `/feed`*:* sends random feed gif
+ • `/poke`*:* sends random poke gif
+ • `/waifu`*:* sends random waifu sticker/image
+ • `/kiss`*:* sends random kiss gif
+ • `/baka`*:* sends random baka gif
+ • `/smug`*:* sends random smug gif
+ • `/foxgirl`*:* sends random foxgirl image
+ • `/feed`*:* sends random feed gif
+ • `/wallpaper`*:* sends random best anime wallpapers.
+ • `/whatanime`*:* to search source of anime reply to photo or gif. 
+  *Extra Fun With Cutiepii:*
+ • `/qu`*:* quote a replied message. Like QuotLyBot
+ • `/aq`*:* get random anime quote.
+ • `/zombies`*:* Search and remove Deleted Accounts from Group.
+
+ • `/plet <your text> `*:* text get funny emojify.
+ • `/tts <your text> `*:*  text to speech.
+ 
+ *Truth And Dare:*
+ • `/Truth`*:* for random truth.
+ • `/dare`*:* for random dare.
+ 
+  *Song:*
+ • `/song <songname artist(optional)> `*:* uploads the song in it's best quality available
+ • `/video <songname artist(optional)> `*:* uploads the video song in it's best quality available
+"""
+
+
+__mod_name__ = "Cutie"
---
+++ Procfile
@@ -0,0 +1,3 @@
+worker: python3 -m Cutiepii_Robot
+web: python3 -m Cutiepii_Robot
+ps:scale worker=1
